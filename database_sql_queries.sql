75ed9701bc1b6581ff02f6d8936b1c0fca945e9c
sreevidya95 <sreevidya95>
2024-11-02 17:17:55 +0000

    adding database queries

---
+++ database_sql_queries.sql
@@ -0,0 +1,18 @@
+-- Databricks notebook source
+CREATE DATABASE IF NOT EXISTS demo;
+
+-- COMMAND ----------
+
+show databases
+
+-- COMMAND ----------
+
+DESCRIBE DATABASE demo;
+
+-- COMMAND ----------
+
+select current_database()
+
+-- COMMAND ----------
+
+
